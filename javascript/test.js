7c5caf10a5ee10949fa82991e6a2357b56af2b83
MarieShin <hotis050@gmail.com>
2021-09-03 18:23:26 +0900

    9/3 javascript 실습

---
+++ javascript/test.js
@@ -0,0 +1,15 @@
+// 새로운 변수 선언 : var
+var a = 8;
+var b = 2;
+var c = 10;
+var na = c % b;
+var d = na * a;
+
+document.write(a + b + c + "<br />");
+document.write((a + b + c) / 3 + "<br />");
+document.write((a + c) / b + "<br />");
+document.write("c % b = " + na + "<br />");
+document.write("(c % b) * a = " + d + "<br />");
+
+// document.write("안녕하세요!");
+// console.log("로그입니다");
