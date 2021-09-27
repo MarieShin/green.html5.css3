9d3ed5d097d5cb3ff256dffb973f7d7e0efa786d
MarieShin <hotis050@gmail.com>
2021-09-27 15:43:58 +0900

    9/27 accuweather js 실습 추가

--- html5_css3/교재외실습/accuweather/js/responsive.js
+++ html5_css3/교재외실습/accuweather/js/responsive.js
@@ -36,6 +36,7 @@ $(function() {
             }
       });    
       
+      /* 하위 메뉴 안보이게 처리 */
       $(".lnb_container").on({
             "mouseleave" : function() {
                   if (gnbFlag) {
@@ -48,4 +49,65 @@ $(function() {
                   }
             }      
       });
+
+      /* 모바일 햄버거 메뉴 클릭 시 */
+
+      $(".trigger").on({
+            "click" : function() {
+                  // 햄버거 메뉴 버튼이 보일 때 (햄버거 메뉴가 visible 상태인가?로 판단)
+                  if ($(".mobile.hamburger").is(":visible")) {
+                        /* 모바일 닫기 버튼 show */
+                        $(".mobile.hamburger").hide();
+                        $(".mobile.close").show();
+
+                        /* 모바일 전체 메뉴 show */
+                        $("#main_header").height(60);
+                        $("#main_content").hide();
+                        $("#main_footer").hide();
+                        $("#search_box").hide();
+
+                        $("#mobile_menu").empty();    // #mobile_menu 하위 태그 초기화
+                        $(".nav li").eq(3).show();    // mobile에서만 보이는 메뉴 2개 추가
+                        $(".nav li").eq(4).show();
+
+                        var nav = $(".nav").clone();
+                        $("#mobile_menu").append(nav);
+                        $("#mobile_menu").show();
+                  } else {
+                        // 단기 메뉴 버튼이 보일 때
+                        /* 모바일 햄버거 버튼 show */
+                        $(".mobile.hamburger").show();
+                        $(".mobile.close").hide();
+
+                        /* 모바일 전체 메뉴 hide */
+                        $("#main_header").height(390);
+                        $("#main_content").show();
+                        $("#main_footer").show();
+                        $("#search_box").show();
+                        $("#mobile_menu").hide();
+                  }
+            }
+      });
+
+      $(window).resize(function() {
+            var width = $(window).width();
+
+            if (width > 767) {
+                  $(".nav li").eq(3).hide();    
+                  $(".nav li").eq(4).hide();
+                  
+                  if ($("#mobile_menu").is(":visible")) {
+                        $(".mobile.hamburger").show();
+                        $(".mobile.close").hide();
+
+                        $("#main_header").height(390);
+                        $("#main_content").show();
+                        $("#main_footer").show();
+                        $("#search_box").show();
+                        
+                        $("#mobile_menu").hide();
+                  }
+            }
+      });
+
 });
